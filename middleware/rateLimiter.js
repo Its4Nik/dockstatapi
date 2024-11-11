3f15710d4d47f6d165b3b7182e0911cada63cc19
Its4Nik <info@itsnik.de>
2024-11-11 06:13:02 +0100

    Delete files

--- middleware/rateLimiter.js
+++
@@ -1,8 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-export const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  limit: 300, // Limit each IP to 300 requests per `window` (here, per 5 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
