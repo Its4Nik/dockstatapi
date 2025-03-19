ede35bacfbf7a2e753d91e4d558137539253a66d
ItsNik <info@itsnik.de>
2025-03-19 14:50:45 +0100

    Feat: Add utils route(s)

---
+++ src/routes/utils.ts
@@ -0,0 +1,45 @@
+import { Elysia, t } from "elysia";
+import {
+  version,
+  authorEmail,
+  authorName,
+  authorWebsite,
+  contributors,
+  dependencies,
+  description,
+  devDependencies,
+  license,
+} from "~/core/utils/package-json";
+import { responseHandler } from "~/core/utils/respone-handler";
+
+export const utilRoutes = new Elysia({ prefix: "/utils" }).get(
+  "/info",
+  async ({ set }) => {
+    try {
+      set.status = 200;
+      return {
+        version,
+        authorEmail,
+        authorName,
+        authorWebsite,
+        contributors,
+        dependencies,
+        description,
+        devDependencies,
+        license,
+      };
+    } catch (error: any) {
+      return responseHandler.error(
+        set,
+        error.message || error,
+        "Error getting DockStatAPI information",
+      );
+    }
+  },
+  {
+    detail: {
+      tags: ["Utils"],
+      description: "Shows general information about DockStatAPI",
+    },
+  },
+);
