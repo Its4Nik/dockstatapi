c24248b8229442e202eff77a470829b8fe4e83a2
Its4Nik <info@itsnik.de>
2025-06-30 20:43:55 +0200

    Lint fixes

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -8,12 +8,12 @@ import { StackHandler } from "./stacks";
 import { CheckHealth } from "./utils";
 
 export const handlers = {
-  BasicDockerHandler,
-  ApiHandler,
-  DatabaseHandler,
-  StackHandler,
-  LogHandler,
-  CheckHealth,
-  Sockets: Sockets,
-  Start: setSchedules(),
+	BasicDockerHandler,
+	ApiHandler,
+	DatabaseHandler,
+	StackHandler,
+	LogHandler,
+	CheckHealth,
+	Sockets: Sockets,
+	Start: setSchedules(),
 };
